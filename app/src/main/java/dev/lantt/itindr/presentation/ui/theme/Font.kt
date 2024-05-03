b2738020a2d418d04113794c9049f7697161cb9d
Matvey Seregin <daymatt@mail.ru>
2024-05-03 16:22:56 +0700

    add fonts and text styles

---
+++ app/src/main/java/dev/lantt/itindr/presentation/ui/theme/Font.kt
@@ -0,0 +1,14 @@
+package dev.lantt.itindr.presentation.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import dev.lantt.itindr.R
+
+val Montserrat = FontFamily(
+    Font(R.font.montserrat_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.montserrat_semibold, FontWeight.SemiBold, FontStyle.Normal),
+    Font(R.font.montserrat_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.montserrat_regular, FontWeight.Normal, FontStyle.Normal),
+)
---
+++ app/src/main/java/dev/lantt/itindr/presentation/ui/theme/TextStyle.kt
@@ -0,0 +1,69 @@
+package dev.lantt.itindr.presentation.ui.theme
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val TypeB24 = TextStyle(
+    fontFamily = Montserrat,
+    fontWeight = FontWeight.Bold,
+    fontSize = 24.sp
+)
+
+val TypeB16 = TextStyle(
+    fontFamily = Montserrat,
+    fontWeight = FontWeight.Bold,
+    fontSize = 16.sp
+)
+
+val TypeB16_n024 = TextStyle(
+    fontFamily = Montserrat,
+    fontWeight = FontWeight.Bold,
+    fontSize = 16.sp,
+    letterSpacing = (-0.24).sp
+)
+
+val TypeB14 = TextStyle(
+    fontFamily = Montserrat,
+    fontWeight = FontWeight.Bold,
+    fontSize = 14.sp
+)
+
+val TypeSB20_015 = TextStyle(
+    fontFamily = Montserrat,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 20.sp,
+    letterSpacing = (0.15).sp
+)
+
+val TypeR16 = TextStyle(
+    fontFamily = Montserrat,
+    fontWeight = FontWeight.Normal,
+    fontSize = 16.sp
+)
+
+val TypeR16_n024 = TextStyle(
+    fontFamily = Montserrat,
+    fontWeight = FontWeight.Normal,
+    fontSize = 16.sp,
+    letterSpacing = (-0.24).sp
+)
+
+val TypeR14 = TextStyle(
+    fontFamily = Montserrat,
+    fontWeight = FontWeight.Normal,
+    fontSize = 14.sp
+)
+
+val TypeR12_n024 = TextStyle(
+    fontFamily = Montserrat,
+    fontWeight = FontWeight.Normal,
+    fontSize = 12.sp,
+    letterSpacing = (-0.24).sp
+)
+
+val TypeM10 = TextStyle(
+    fontFamily = Montserrat,
+    fontWeight = FontWeight.Medium,
+    fontSize = 10.sp
+)
