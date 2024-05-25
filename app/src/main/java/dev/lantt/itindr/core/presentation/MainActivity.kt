eab42defc898d401bc6cb164efadafc6e15da465
Matvey Seregin <daymatt@mail.ru>
2024-05-26 01:15:04 +0700

    add clearing focus and removing soft keyboard

--- app/src/main/java/dev/lantt/itindr/core/presentation/MainActivity.kt
+++ app/src/main/java/dev/lantt/itindr/core/presentation/MainActivity.kt
@@ -1,6 +1,11 @@
 package dev.lantt.itindr.core.presentation
 
+import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,10 +14,13 @@ import com.github.terrakok.cicerone.NavigatorHolder
 import dev.lantt.itindr.R
 import dev.lantt.itindr.core.presentation.navigation.ITindrNavigator
 import org.koin.android.ext.android.inject
+import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
     private val navigatorHolder: NavigatorHolder by inject()
     private val navigator = ITindrNavigator(this, R.id.fragmentHost)
+
+    private var downGestureX = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +31,48 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun dispatchTouchEvent(event: MotionEvent?): Boolean {
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> downGestureX = event.rawX.toInt()
+            MotionEvent.ACTION_UP -> {
+                val focusedView = currentFocus
+                if (focusedView is EditText) {
+                    val x = event.rawX.toInt()
+                    val y = event.rawY.toInt()
+
+                    if (abs(downGestureX - x) > 5) {
+                        return super.dispatchTouchEvent(event)
+                    }
+
+                    val outRect = Rect()
+                    focusedView.getGlobalVisibleRect(outRect)
+                    if (!outRect.contains(x, y)) {
+                        focusedView.clearFocus()
+                        var touchTargetIsEditText = false
+
+                        focusedView.rootView.touchables.forEach { touchable ->
+                            if (touchable is EditText) {
+                                val clickedViewRect = Rect()
+                                touchable.getGlobalVisibleRect(clickedViewRect)
+                                if (clickedViewRect.contains(x, y)) {
+                                    touchTargetIsEditText = true
+                                    return@forEach
+                                }
+                            }
+                        }
+
+                        if (!touchTargetIsEditText) {
+                            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                            inputMethodManager.hideSoftInputFromWindow(focusedView.windowToken, 0)
+                        }
+                    }
+                }
+            }
+        }
+
+        return super.dispatchTouchEvent(event)
     }
 
     override fun onResumeFragments() {
